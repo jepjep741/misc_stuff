af2b9e29091962ea9b28db33f01543740350d621
jepjep741 <89215038+jepjep741@users.noreply.github.com>
2023-03-26 20:20:29 +0300

    Initial commit

---
+++ webserver.py
@@ -0,0 +1,18 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        print(post_data.decode('utf-8'))
+        self.send_response(200)
+        self.end_headers()
+
+def run(server_class=HTTPServer, handler_class=MyServer, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
